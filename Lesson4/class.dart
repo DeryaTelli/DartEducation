2a8926a8e2348cf58918106b243c4e8e924c11e7
Mobilyst <2002derya2002@gmail.com>
2023-12-22 14:27:44 +0300

    lesson 4 class and null safeyt

---
+++ Lesson4/class.dart
@@ -0,0 +1,124 @@
+void main() {
+  //musteri adi var , parasi var , yasi var vb.
+  final int cusMoney = 5;
+  final String cusName = 'ahmet';
+  final int cusAge = 13;
+  //bu musterinin yasi 10 dan buyukse islem yap
+  controlCus(cusMoney);
+  // yeni musteri geliyor yine ayni alanlar
+  final int cusMoney1 = 45;
+  final String cusName1 = 'derya';
+  final int cusAge1 = 34;
+  controlCus(cusMoney1);
+
+  // musterilerin sehirlerini eklemelisin
+  // musterleri gruplasam yani bir kumelesem ve bunlar ayni ozellikleri ayni sekilde bana gosterebilse
+  // class olusturma yapiyoruz yani
+
+  int? newmoney;
+  // ? koyma sebebimiz dart bir degisken tanimlarken onun ne oldugunu bilmek istiyor
+  // yani baslangic degeri veremden bir degisken tanimlarsam hata alaiyorum
+  // cunku dart hic bir nesneyi basi bos tanimlamami istemiyor
+  // bunu duzeltmek icin;
+  // ya default deger vericem , ya degerini atayacagim yada burada oldugu gibi ? koyucagim
+  print(newmoney);
+
+  int? newMoney2; // bu kod daha sagliklidir su yontemden
+  // print(newMoney2!+50); o unlemi ekledigim zaman ben kesinlikle null gelmicek demis oluyorum
+  // bu yuzden bu yontem cok tercih edilmemeli
+  if (newMoney2 != null) {
+    print(newMoney2 + 50);
+  }
+
+// bankaya 3 tane musteri gelir birinin 100 tlsi var digernin hesabi hic yok
+//digerinin 0 tlsi var
+// hesabi olmayana hesap acalim
+// 0 tlsi olani kov 100 tlsi olana hosgeldiniz de
+// yeni bir method olsun bu method hesabi olmayanlari ve parasi 0 olanlari yok saysin
+// para verdiklerimize ekranda true yazalim
+  List<int?> customers = [100, null, 0];
+  for (var item in customers) {
+    if (item != null) {
+      if (item > 0) {
+        print('welcome to our bank');
+      } else {
+        print('please quit');
+      }
+    } else {
+      print('open new account');
+    }
+
+    //------------------
+    bool result =
+        controlMoney(item) == null ? false : true; // conditional expression
+    print(result);
+    // eger item degeri null ise false calisicak
+    // eger item degeri null degil ise true calisacaktir
+  }
+
+  print('--------' * 10); // yandaki isaretten 10 kere yazar
+
+  User a = new User('derya', 45, age: 21, city: 'Mersin');
+  // constructor kullanarak nesne yarattim javaya benziyor
+  print(a.toString());
+  User d = new User('deraa', 100);
+  print(d.toString());
+  // musteri son gelen kisinin cityisine gosre kampanya yapacaktir eger istebul ise
+  if (d.city != null) {
+    if (d.city == 'istanbul') {
+      print('you won campaign');
+    }
+  } else {
+    print('not avaible your city information ');
+  }
+}
+
+int? controlMoney(int? money) {
+  // methoda ? ekleme sebebim null dondurebilecegim icin eger null koymasaydim hata alicaktim
+// bunun degeri hic null gelmicek demek oluyor bundan kaynaklda hata olusuyor
+  if (money != null && money > 0) {
+    return money;
+  }
+  //eger geriye deger donduren bir methodda return yazmasaniz ve bumethod da
+  // null dondurma durumu varsa dart nullu otomatik dondurur yazmana gerek kalmaz
+  // ama okuma acisindan yazabilirsin
+  //if (money == null || money == 0) {
+  //  return null;
+  //}
+}
+
+void controlCus(int value) {
+  if (value > 10) {
+    print('might do shopping');
+  } else {
+    print('might not do shopping');
+  }
+}
+
+// adi olmak zorunda
+// parasi olmak zorunda
+//yasini vermeyebilir
+// sehrini vermeyebilir
+class User {
+  //ozellikleri tanimlamam gerekiyor
+  late final String name; // late demek bu deger sonradan dolacaktir demek
+  // ilk basta degeri null anlami yok demek bu yuzden dart bunu tanimlamami istiyor
+  late final int money;
+  late final int? age;
+  // olmayabilir degerlere ? ekliyorum null gelebilir demek istedigim icin
+  late final String? city;
+  //olmayabilir degerlere ? ekliyorum null gelebilir demek istedigim icin
+  // gelemeyen parametleri opsiyenel gostermek yani girsemde olur oldugu icin {} parantez icine aliyorum
+  User(String name, int money, {int? age, String? city}) {
+// constructor
+    this.name = name;
+    this.money = money;
+    this.age = age;
+    this.city = city;
+  }
+
+  @override
+  String toString() {
+    return 'name: ${name} money: ${money} age: ${age} city: ${city}';
+  }
+}
