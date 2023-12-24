1e0158eb0caa0b16f9e3b62ad4029971a86c7372
Mobilyst <2002derya2002@gmail.com>
2023-12-24 15:57:32 +0300

    lesson 4 parent class , child class and abstract class

---
+++ Lesson4/class_extends.dart
@@ -0,0 +1,60 @@
+void main() {
+  User a = new User('Derya', 35);
+  // yukaridaki kodu olusturmanin diger farkli yontemi ise sudur
+  // final a =User('Derya',35);
+  a.sayMoneyWithCompanyName();
+  final userS = SpecialUser('deraa', 100, 50, 30);
+  print(userS.calculate);
+  print(userS.money); // moneyi degistirmiyor indirimli fiyati gosteriyor
+ 
+}
+
+// hepsinin paralarini gormelerini istiyorum benim bnka ismim ile
+//hepsinde ayni methodun olmasini ve farkli ozzellukleri olmasini istiyorsam abstract bir class olusturmam gerekecektir
+// abstract classla nesne olusturamazsin
+abstract class IUser {
+  final String name;
+  final int money;
+  IUser(this.name, this.money);
+  void sayMoneyWithCompanyName() {
+    print('ahmet- ${money} parasi var');
+  }
+}
+
+class User extends IUser {
+  // extends etmek aslinda parenti childa baglamaktir
+  //Iuser parent user classida childdir ve child parentin her ozelligine erisebilir
+  // methodlarinida kendine gore degistirebilir
+
+  // final String name;
+  // final int money;
+  // User(this.name, this.money) : super(name, money);
+  // yukaridaki yontemlede yazabilirsin ama genelde tercih edilen yontem budur
+  User(String name, int money) : super(name, money);
+  // bu islem yukaridakiyle aynidir
+}
+
+class BankUser extends IUser {
+  final int bankCode;
+  BankUser(String name, int money, this.bankCode) : super(name, money);
+}
+
+class SpecialUser extends IUser {
+  final int bankCode;
+  late final  int _discount;
+  SpecialUser(String name, int money, this.bankCode, int discount)
+      : super(name, money) {
+    _discount = discount;
+  }
+  // indirimli fiyati ne kadar
+  int get calculate => money - (money ~/ discount);
+
+  // geri deger donduren methodlarda tek islem varsa arrow functiona dondurmek daha yararlidir
+  // property yani deger almiyorsa methot getter yapariz
+  //int calculate(int discount) {
+  // return money - (money ~/ discount);
+  //}
+
+  int get discount => _discount;
+  set discount(int discount) => _discount = discount;
+}
--- Lesson4/user_model2.dart
+++ Lesson4/user_model2.dart
@@ -1,12 +1,39 @@
 // usermodel darttaki  gibi bide buradaki gibi class olusturabilirsin
 class User2 {
   final String name;
-   int money;
-  // final degiskenini kaldirdik cunku money degisen bir deger  
+  int _money; // private yaptim money degiskenimi
+  // final degiskenini kaldirdik cunku money degisen bir deger
   final int? age;
   final String? city;
   late final String userCode;
-  User2(this.name, this.money, {this.age, this.city}) {
+  User2(this.name, this._money, {this.age, this.city}) {
     userCode = (city ?? 'istanbul') + name;
+  }
+
+  // private degerime baska classlardan erismek istiyorsam onlarin getter ve setter
+  //methodlarini yazmam gerekiyor
+  // int getMoney() {
+  // return _money;
+  // }
+  // asagidaki methodu boylede olusturabilirsin
+  int get money => _money;
+
+  //setMoney(int money) {
+  //  _money = money;
+  //}
+  // asagidaki methodu boylede olusturursun
+  //set money(int money){
+  //  _money = money;
+  //}
+
+  //eger girilen deger null gelebilirse napilmali
+  set money(int? money) {
+    if (money == null) {
+      _money = 0;
+    } else {
+      _money = money;
+    }
+    // bu kodu yazmanin diger bir yolu ise
+    // _money= money == null ? 0 : money; 'kisa if yapisi '
   }
 }
